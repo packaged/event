2f1593c6e80ff81f6510c2e01dd895dd15529adc
Brooke Bryan <brooke@bajb.net>
2019-04-17 08:33:16 +0100

    Remove object type hints from Trusted Publisher Channel

--- src/Channel/TrustedPublisherChannel.php
+++ src/Channel/TrustedPublisherChannel.php
@@ -7,27 +7,27 @@ class TrustedPublisherChannel extends Channel
 {
   private $_publisher;
 
-  public function __construct(string $name, object $publisher)
+  public function __construct(string $name, $publisher)
   {
     parent::__construct($name);
     $this->_publisher = $publisher;
   }
 
-  public function verifyPublisher($publisher)
+  protected function _assertPublisher($publisher)
   {
-    return $publisher === $this->_publisher;
-  }
-
-  public function triggerAndConsume(Event $event, bool $throwExceptions = false, object $publisher = null)
-  {
-    if(!$this->verifyPublisher($publisher))
+    if($publisher !== $this->_publisher)
     {
       throw new \RuntimeException("You are not permitted to publish to this channel", 403);
     }
+  }
+
+  public function triggerAndConsume(Event $event, bool $throwExceptions = false, $publisher = null)
+  {
+    $this->_assertPublisher($publisher);
     return parent::triggerAndConsume($event, $throwExceptions);
   }
 
-  public function trigger(Event $event, bool $throwExceptions = false, object $publisher = null)
+  public function trigger(Event $event, bool $throwExceptions = false, $publisher = null)
   {
     iterator_to_array($this->triggerAndConsume($event, $throwExceptions, $publisher));
     return $this;
